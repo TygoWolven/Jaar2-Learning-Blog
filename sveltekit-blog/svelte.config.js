2452b4291c6a71da5a616bcda4390fd2c0eadf27
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-09-16 17:35:26 +0200

    .

--- sveltekit-blog/svelte.config.js
+++ sveltekit-blog/svelte.config.js
@@ -2,11 +2,8 @@ import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 import { mdsvex } from 'mdsvex'
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md'],
-}
+import { escapeSvelte } from 'mdsvex'
+import { getHighlighter } from 'shiki'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,6 +12,22 @@ const config = {
 	kit: {
 		adapter: adapter()
 	}
+}
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await getHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			return `{@html \`${html}\` }`
+		}
+	},
 }
 
 export default config
